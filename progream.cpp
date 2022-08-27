04da0891ad7de36eed7fc9632dcf13bea31c5224
ARSLAN ASLAM <105386182+shabi6342@users.noreply.github.com>
2022-08-27 08:19:10 +0500

    Update progream.cpp

--- progream.cpp
+++ progream.cpp
@@ -1,7 +1,10 @@
+//loop progrem write now
 #include <iostream>
 using namespace std;
 
 int main() {
-  cout << "Hello World!";
-  return 0;
+    
+for(int i=1; i<10; i++){
+    cout<<i<<endl;
+}  return 0;
 }
