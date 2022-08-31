88db20f74603ea8441950775220e1a2c3e0df5ed
ARSLAN ASLAM <105386182+shabi6342@users.noreply.github.com>
2022-08-31 23:40:33 +0500

    dd

--- fastpro.cpp
+++ fastpro.cpp
@@ -4,6 +4,17 @@ using namespace std;
 
 int main()
 {
-    cout << "hello world"<< endl;    
+	cout<<endl;
+	cout<<endl;
+	cout << "\t""           |__________|"<< endl;    
+	cout<<"   ____________________________________________"<<endl;
+	    cout << "\t""/____________|""|_____________|"<< endl;
+	    cout << "\t""            /    |"<< endl;  
+		cout << "\t""           /     |"<< endl;  
+		cout << "\t""          /      |"<< endl;  
+		cout << "\t""         /       |"<< endl;  
+		cout << "\t""        /        |"<< endl;  
+		cout << "\t""       /         |"<< endl;  
+		cout << "\t""      /__________|"<< endl;    
  return 0;
 }
