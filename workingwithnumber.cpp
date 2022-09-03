3395cbfea37670381f8254f80cea9ce38f3f875b
ARSLAN ASLAM <105386182+shabi6342@users.noreply.github.com>
2022-09-04 00:53:53 +0500

    ejd

--- workingwithnumber.cpp
+++ workingwithnumber.cpp
@@ -4,9 +4,17 @@ int main()
 {
 	
 //	Working with number started now 
-	
-	
-	
+		
+	cout<< 12+23;
+	cout<<endl;
+	cout<<19%2;
+	cout<<endl;
+	cout<<24*5;
+	cout<<endl;
+	cout<<10293/23;
+	cout<<endl;	
+	cout<<(12+21)*123/23;
+	cout<<endl;	
 	return 0;
 	
 }
