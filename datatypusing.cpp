144df1d9a53d99e66fdcef803ff9365fda821676
ARSLAN ASLAM <105386182+shabi6342@users.noreply.github.com>
2022-09-03 22:11:34 +0500

    not work  rogream now

---
+++ datatypusing.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main(){
+	char name, namelike;
+	name ="d";
+	int nameage;
+	nameage=35;
+	namelike="Shabi";
+	cout<<"There once was a man named ";
+	cout<<name_1;
+	cout<<endl;
+	cout<<"He was 35 year old";
+	cout<<endl;
+	cout<<"He liked the name john ";
+	cout<<endl;
+	cout<<"But did not like bring 35";
+
+	
+	
+	
+	return 0;
+}
---
+++ threepro.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(
+){
+	cout<<"There once was a man named Shoain";
+	cout<<endl;
+	cout<<"He was 35 year old";
+	cout<<endl;
+	cout<<"He liked the name john ";
+	cout<<endl;
+	cout<<"But did not like bring 35";
+
+	
+	
+	
+	return 0;
+}
