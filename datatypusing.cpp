23daf4e0964d0b3c5d409ffff86ae42cbecc904a
ARSLAN ASLAM <105386182+shabi6342@users.noreply.github.com>
2022-09-03 22:48:01 +0500

    we

--- datatypusing.cpp
+++ datatypusing.cpp
@@ -1,22 +1,19 @@
 #include <iostream>
 using namespace std;
 int main(){
-	char name, namelike;
-	name ="d";
+	string name, namelike;
+	name ="Shoaib Ahmad";
 	int nameage;
 	nameage=35;
 	namelike="Shabi";
 	cout<<"There once was a man named ";
-	cout<<name_1;
+	cout<<name;
 	cout<<endl;
-	cout<<"He was 35 year old";
+	cout<<"He was " <<nameage<< " year old";
 	cout<<endl;
-	cout<<"He liked the name john ";
+	cout<<"He liked the name "<<namelike;
 	cout<<endl;
 	cout<<"But did not like bring 35";
-
-	
-	
 	
 	return 0;
 }
