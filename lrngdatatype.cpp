211e3dfbb66495519ea3d4d9c26510751a308ee2
ARSLAN ASLAM <105386182+shabi6342@users.noreply.github.com>
2022-09-04 00:23:49 +0500

    trsdg

---
+++ lrngdatatype.cpp
@@ -0,0 +1,31 @@
+//This file lesson is  type a Data type lerning now 
+#include <iostream>
+using namespace std;
+int main(){
+	
+	
+	char grade= 'A';
+	cout<< grade;
+	cout<<endl;
+	string paraghrph ="Hi! dear sir mpidjcuufd wkmsdoiu kpip0-ewhu7e";
+	cout<<paraghrph;
+	cout<<endl;
+	int holenumbertype = 237;
+	cout<<holenumbertype;
+	cout<<endl;
+	double doubletypedate =122.2232;
+	cout<<doubletypedate<<endl;
+	bool truedata= true;
+	cout << truedata << endl;
+	bool falsedata= 0;
+	cout << falsedata << endl;
+	
+	
+
+
+
+	
+	
+	
+	return 0;
+}
---
+++ workingwithstring.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main(){
+	string phasse = "Shoaib Ahmad";
+	phasse[5]='Z';
+	cout<<phasse<<endl;
+	string phae = "Zafer Akrem working now";
+	cout<<phae.find("noe", 1)<<endl;
+
+
+	return 0;
+}
