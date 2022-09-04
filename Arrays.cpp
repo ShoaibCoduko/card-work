56112d4001a315d0d532234680c51bf358b7c665
ARSLAN ASLAM <105386182+shabi6342@users.noreply.github.com>
2022-09-04 22:14:53 +0500

    ywge

--- Arrays.cpp
+++ Arrays.cpp
@@ -1,8 +1,15 @@
 #include<iostream>
+#include <cmath>
 using namespace std;
 int main()
 {
 	cout<<"we are leraning arrays in c++ language\n\n\n\n";
+	 int luckynums[]={2, 3, 12, 122, 4, 5, 77, 9, 19, 100};
+	 cout<<luckynums[2]<<endl;
+	 cout <<"we are changes the value in this arrays number value \n\n\n";
+	 luckynums[3]=33;
+	 cout<<luckynums[3]<<endl;
+	
 	
 	return 0;
 }
---
+++ functioninc++.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	cout<<"hello dd\n\n";
+	
+	return 0;
+}
