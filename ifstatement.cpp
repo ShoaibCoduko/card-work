6203ecc708fd59665d3d334bff8885616321cac0
ARSLAN ASLAM <105386182+shabi6342@users.noreply.github.com>
2022-09-05 19:29:54 +0500

    if tak complete c++ language

--- ifstatement.cpp
+++ ifstatement.cpp
@@ -3,6 +3,30 @@ using namespace std;
 int main()
 {
 	cout<<"hello world\t\n";
+	bool flaseint = 0;
+	bool trueint = 1;
+	if(flaseint && trueint){
+		cout<<"This is a false or teue  value number\n";
+	}
+	else{
+		cout<<"this is not false value number\n";
+	}
+	
+	int age;
+	cin>>age;
+	if(age <= 10){
+		for(int o=0; o<=age; o++){
+			cout<<o<<" : 3"<<endl;
+		}
+	}	else if(age == 12 || age == 13 || age == 14 || age == 15){
+		cout<<"you are "<<age<<" year old but not under the age  ten year old \n";
+	}
+	else if(age <= 16){
+		cout<<"you 	are "<<age<<"year old but you are not sectel  \n";
+	}
+	else{
+		cout<<"you are not correct value \n";
+	}
 	
 	return 0;
 }
---
+++ table99.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+#include<cmath>
+ using namespace std;
+ int main()
+ {
+ 
+ 	int talr;
+ 	
+ 	cout<<"Fast Table Number Value Enter"<<endl<<endl<<endl;
+ 	cin>>talr;
+ 	cout<<endl<<endl<<endl<<endl;
+ 	for(int i=1; i<=10; i++)
+ 	{
+ 		cout<<talr<<"\t x \t "<<"\t"<<i<<"\t =\t "<<"\t"<<talr*i<<endl;
+	 }
+	 	int talr2;
+	 	
+ 	cout<<"Second Table Number Value Enter"<<endl<<endl<<endl;
+ 	cin>>talr2;
+ 	cout<<endl<<endl<<endl<<endl;
+ 	for(int i=1; i<=10; i++)
+ 	{
+ 		cout<<talr2<<"\t x \t "<<"\t"<<i<<"\t =\t "<<"\t"<<talr2*i<<endl;
+	 }
+	 	int talr3;
+	 	
+ 	cout<<"third Table Number Value Enter"<<endl<<endl<<endl;
+ 	cin>>talr3;
+ 	cout<<endl<<endl<<endl<<endl;
+ 	for(int i=1; i<=10; i++)
+ 	{
+ 		cout<<talr3<<"\t x \t "<<"\t"<<i<<"\t =\t "<<"\t"<<talr3*i<<endl;
+	 }
+ 	return 0;
+ }
