4e47273d875fff94510dee281800cffd4ed47d34
ARSLAN ASLAM <105386182+shabi6342@users.noreply.github.com>
2022-09-06 19:37:40 +0500

    d

--- expondfunction.cpp
+++ expondfunction.cpp
@@ -1,7 +1,16 @@
 #include<iostream>
 using namespace std;
+int power(int basenum, int ponum){
+	int result = 1;
+	for(int i = 0; i<ponum; i++)
+	{
+		result = result*ponum;
+	}
+	return result;
+}
 int main()
 {
 	cout<<"Exponent Function\n";
+	cout<<power(1, 2);
 	return 0;
 }
