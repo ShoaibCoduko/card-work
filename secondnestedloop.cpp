d77b26f8524a4ac1cf089f09c3d935ba4f6e1da2
ARSLAN ASLAM <105386182+shabi6342@users.noreply.github.com>
2022-09-07 00:08:17 +0500

    last day com[le

--- secondnestedloop.cpp
+++ secondnestedloop.cpp
@@ -3,6 +3,18 @@ using namespace std;
 int main()
 {
 	cout<<"Second Arrays & Nested loops\n";
+	int numbgrid[5][2] = {
+						{1, 8},
+						{0, 12},
+						{7, 6},
+						{2, 1},
+						{3, 4}
+						};
+		for(int j = 0; j<2; j++){
+			cout<<numbgrid[i][j];
+		}
+		cout<<endl;
+	}
 	
 	return 0;
 }
