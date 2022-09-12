0bda56b25da064044e2613963d9160747a8696ce
ARSLAN ASLAM <105386182+shabi6342@users.noreply.github.com>
2022-09-13 01:03:38 +0500

    dfd

---
+++ oop.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+
+using namespace std;
+class clas{
+	public:
+	 string studentname;
+	 string studesubject;
+	 int rollnumber;
+	 int studmak;
+	  
+};
+int main(){
+	clas student1;
+	student1.studentname;
+	student1.studesubject;
+	student1.rollnumber;
+	student1.studmak;
+cin>>student1.studentname;
+cin>>student1.studesubject;
+cin>>student1.rollnumber;
+cin>>student1.studmak;
+	
+	
+		
+	
+	return 0;
+}
